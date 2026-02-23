30af54bb7fe0beeda6abc6577bd95f104537169b
victo-silvers <silversvicto@gmail.com>
2026-02-23 14:48:20 +0530

    Created UserRepository

---
+++ src/main/java/com/sahil/taskmanager/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.sahil.taskmanager.repository;
+
+import com.sahil.taskmanager.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}

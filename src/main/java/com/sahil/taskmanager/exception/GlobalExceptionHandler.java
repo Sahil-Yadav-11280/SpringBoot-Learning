c6fc0d6e9deda4bd4a58d1fc0fe9f2030c8bb671
victo-silvers <silversvicto@gmail.com>
2026-02-28 16:39:37 +0530

    created a GlobalExceptionHandler to handle invalid arguments exception

---
+++ src/main/java/com/sahil/taskmanager/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.sahil.taskmanager.exception;
+
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.View;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationException(MethodArgumentNotValidException ex){
+        Map<String ,String > errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName , errorMessage);
+        });
+
+        return new ResponseEntity<>(errors , HttpStatus.BAD_REQUEST);
+    }
+
+}
